cb0bf71ad6fc5f04cc85a7d72d580fad5c8d1edb
Chinhphu <doraemon.com56@gmail.com>
2024-05-23 13:47:52 +0700

    Định nghĩa xong Text Style

---
+++ lib/src/shared/styles.dart
@@ -0,0 +1,31 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+// Text style
+
+const TextStyle h1 = TextStyle(
+  fontSize: 32,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle h2 = TextStyle(
+  fontSize: 24,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle h3 = TextStyle(
+  fontSize: 16,
+  fontWeight: FontWeight.w700,
+);
+
+const TextStyle min_cap = TextStyle(
+  fontSize: 10,
+  fontWeight: FontWeight.w400,
+);
+
+const TextStyle bd_text = TextStyle(
+  fontSize: 12,
+  fontWeight: FontWeight.w400,
+);
