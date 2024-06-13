dd88c473f4600cbf5691f1bc240cde83dc407f8b
RiTaka <huyentran0157@gmail.com>
2024-06-14 00:59:46 +0700

    ?

--- lib/src/pages/change_profile.dart
+++ lib/src/pages/change_profile.dart
@@ -8,7 +8,6 @@ import 'package:yogi_application/src/widgets/box_input_field.dart';
 import 'package:yogi_application/src/widgets/dropdown_field.dart';
 import 'package:yogi_application/src/widgets/box_button.dart';
 import 'package:yogi_application/src/pages/change_BMI.dart';
-import 'package:yogi_application/src/models/user_profile.dart'; // Import your UserProfile model
 import 'package:yogi_application/src/services/api_service.dart'; // Import your fetchUserProfiles function
 
 class ChangeProfilePage extends StatefulWidget {
