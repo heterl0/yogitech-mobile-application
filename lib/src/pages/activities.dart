b2d53caeeffe341ba113ef3e107ce2e2d8c8c3c2
Hieu Le Van <lehieu99666@gmail.com>
2024-06-17 17:55:33 +0700

    refactor: Update Activities page to load events from API
    This commit modifies the `Activities` page to load events from the API using the `EventService` class. It adds the necessary imports and initializes a list of events in the `_ActivitiesState` class. The `initState` method is updated to call the `_loadEvents` method, which makes an API request to fetch the events and updates the state with the retrieved data. The `itemCount` property in the `GridView.builder` is updated to use the length of the `_events` list, ensuring that the correct number of cards is displayed. The `title`, `caption`, and `remainingDays` properties of the `CustomCard` widget are updated to use the corresponding properties of the events in the `_events` list. This change improves the functionality of the `Activities` page by dynamically loading events from the API.

--- lib/src/pages/activities.dart
+++ lib/src/pages/activities.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+import 'package:yogi_application/api/event/event_service.dart';
 import 'package:yogi_application/src/custombar/appbar.dart';
+import 'package:yogi_application/src/models/event.dart';
 import 'package:yogi_application/src/pages/event_detail.dart';
-import 'package:yogi_application/src/shared/app_colors.dart';
-import 'package:yogi_application/src/shared/styles.dart';
 import 'package:yogi_application/src/widgets/card.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:yogi_application/utils/formatting.dart';
 
 class Activities extends StatefulWidget {
   @override
@@ -14,6 +15,14 @@ class Activities extends StatefulWidget {
 
 class _ActivitiesState extends State<Activities> {
   // Biến trạng thái để lưu trữ nội dung hiện tại
+
+  List<dynamic> _events = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadEvents();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +34,18 @@ class _ActivitiesState extends State<Activities> {
       ),
       body: _buildBody(context),
     );
+  }
+
+  Future<void> _loadEvents() async {
+    try {
+      final events = await getEvents();
+      setState(() {
+        _events = events;
+      });
+    } catch (e) {
+      // Handle errors, e.g., show a snackbar or error message
+      print('Error loading activities: $e');
+    }
   }
 
   Widget _buildBody(BuildContext context) {
@@ -59,24 +80,23 @@ class _ActivitiesState extends State<Activities> {
           mainAxisSpacing: 8.0,
           childAspectRatio: 3 / 2, // Aspect ratio of each card
         ),
-        itemCount: 6, // Number of cards
-        itemBuilder: (context, index) {
-          final title = '${trans.event} ${index + 1}';
-          final caption = 'Caption ${index + 1}';
-          final subtitle = '${5 - index} ${trans.daysLeft}';
+        itemCount: _events.length, // Number of cards
 
+        itemBuilder: (context, index) {
           return CustomCard(
-            title: title,
-            caption: caption,
-            subtitle: subtitle,
+            title: _events[index].title,
+            caption:
+                "Number of participants: ${_events[index].event_candidate.length}",
+            subtitle: checkDateExpired(
+                _events[index].start_date, _events[index].expire_date),
             onTap: () {
               pushWithoutNavBar(
                 context,
                 MaterialPageRoute(
                   builder: (context) => EventDetail(
-                    title: title,
-                    caption: caption,
-                    remainingDays: subtitle,
+                    title: _events[index].title,
+                    caption: _events[index].id.toString(),
+                    remainingDays: _events[index].expire_date,
                   ),
                 ),
               );
--- lib/utils/formatting.dart
+++ lib/utils/formatting.dart
@@ -14,3 +14,25 @@ String cleanApiUrl(String? url) => (url ?? '')
     .trim()
     .replaceFirst(RegExp(r'/api/v1'), '')
     .replaceFirst(RegExp(r'/$'), '');
+
+String checkDateExpired(String startDateStr, String dateStr) {
+  DateTime targetDate = DateTime.parse(dateStr);
+  DateTime startDate = DateTime.parse(startDateStr);
+  // // Format the date
+  // String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(targetDate);
+  // print('Formatted date: $formattedDate');
+
+  // Calculate the difference
+  DateTime now = DateTime.now();
+  Duration startDifferent = startDate.difference(now);
+  if (!startDifferent.isNegative) {
+    return 'The event is not start.';
+  }
+  Duration difference = targetDate.difference(now);
+
+  if (difference.isNegative) {
+    return 'The date has passed.';
+  } else {
+    return '${difference.inDays} days remaining.';
+  }
+}
