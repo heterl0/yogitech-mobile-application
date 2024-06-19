735cefd48091fbb151b0c037831303a717055dcb
Hieu Le Van <lehieu99666@gmail.com>
2024-06-19 18:08:06 +0700

    feat: Add share functionality to profile page

--- lib/api/auth/auth_service.dart
+++ lib/api/auth/auth_service.dart
@@ -56,7 +56,6 @@ Future<dynamic> register(RegisterRequest data) async {
     Response response = await DioInstance.post(url, data: bodyData);
 
     if (response.statusCode == 201) {
-      print(123);
       return {'status': response.statusCode, 'message': response.data};
     }
   } catch (e) {
