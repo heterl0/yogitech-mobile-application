a5512298df5eb8304a464d3e758102e757d63512
Duy Nguyen <tronhoc12namlien@gmail.com>
2024-06-22 20:28:21 +0700

    arrow back button

--- lib/src/pages/forgot_password.dart
+++ lib/src/pages/forgot_password.dart
@@ -9,8 +9,14 @@ import 'package:YogiTech/src/widgets/box_input_field.dart';
 import 'package:YogiTech/src/widgets/box_button.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-class ForgotPasswordPage extends StatelessWidget {
+class ForgotPasswordPage extends StatefulWidget {
+  @override
+  _ForgotPasswordPageState createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
   final TextEditingController emailController = TextEditingController();
+  bool isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,77 +27,99 @@ class ForgotPasswordPage extends StatelessWidget {
         ? 'assets/images/login-sign.png'
         : 'assets/images/login-sign_light.png';
     return Scaffold(
-      backgroundColor: theme.colorScheme.onSecondary,
-      body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage(imageAsset),
-            fit: BoxFit.fitWidth,
-            alignment: Alignment.topCenter,
-          ),
+      extendBodyBehindAppBar:
+          true, // Đảm bảo AppBar trong suốt không ảnh hưởng đến nội dung
+      appBar: AppBar(
+        leading: IconButton(
+          padding: EdgeInsets.all(12.0),
+          iconSize: 32.0,
+          icon: Icon(Icons.arrow_back,
+              color: Colors.white), // Màu của biểu tượng mũi tên là màu trắng
+          onPressed: () {
+            Navigator.pop(context);
+          },
         ),
-        padding: EdgeInsets.symmetric(horizontal: 20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Text(
-              trans.forgotPasswordTitle,
-              style: h1.copyWith(color: theme.colorScheme.onPrimary),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(height: 16.0),
-            BoxInputField(
-              controller: emailController,
-              placeholder: trans.email,
-            ),
-            SizedBox(height: 20.0),
-            BoxButton(
-              title: trans.request,
-              style: ButtonStyleType.Primary,
-              state: ButtonState
-                  .Enabled, // hoặc ButtonState.Disabled để test trạng thái disabled
-              onPressed: () {
-                _handleResetPassword(context, emailController.text);
-              },
-            ),
-            SizedBox(height: 10.0),
-            Row(children: <Widget>[
-              Expanded(
-                  child: Divider(
-                color: text,
-              )),
-              Padding(
-                padding: EdgeInsets.symmetric(horizontal: 8.0),
-                child: Text(
-                  trans.orSignInWith,
-                  style: bd_text.copyWith(color: text),
-                ),
+        backgroundColor: Colors.transparent, // Nền của AppBar trong suốt
+        elevation: 0,
+      ),
+      backgroundColor: theme.colorScheme.onSecondary,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(imageAsset),
+                fit: BoxFit.fitWidth,
+                alignment: Alignment.topCenter,
               ),
-              Expanded(child: Divider(color: text)),
-            ]),
-            Row(
+            ),
+            padding: EdgeInsets.symmetric(horizontal: 20.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
                 Text(
-                  trans.dontHaveAccount,
-                  style: bd_text.copyWith(color: text),
+                  trans.forgotPasswordTitle,
+                  style: h1.copyWith(color: theme.colorScheme.onPrimary),
+                  textAlign: TextAlign.center,
                 ),
-                TextButton(
-                  onPressed: () {
-                    // Xử lý sự kiện khi nhấn vào "Sign up"
-                    // Chuyển đến trang đăng ký
-                    Navigator.pushNamed(context, AppRoutes.signup);
-                  },
-                  child: Text(
-                    trans.signUp,
-                    style: h3.copyWith(color: primary),
+                SizedBox(height: 16.0),
+                BoxInputField(
+                  controller: emailController,
+                  placeholder: trans.email,
+                ),
+                SizedBox(height: 20.0),
+                BoxButton(
+                  title: trans.request,
+                  style: ButtonStyleType.Primary,
+                  state: isLoading ? ButtonState.Disabled : ButtonState.Enabled,
+                  onPressed: isLoading
+                      ? null
+                      : () {
+                          _handleResetPassword(context, emailController.text);
+                        },
+                ),
+                SizedBox(height: 10.0),
+                Row(children: <Widget>[
+                  Expanded(
+                      child: Divider(
+                    color: text,
+                  )),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    child: Text(
+                      trans.orSignInWith,
+                      style: bd_text.copyWith(color: text),
+                    ),
                   ),
+                  Expanded(child: Divider(color: text)),
+                ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      trans.dontHaveAccount,
+                      style: bd_text.copyWith(color: text),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pushNamed(context, AppRoutes.signup);
+                      },
+                      child: Text(
+                        trans.signUp,
+                        style: h3.copyWith(color: primary),
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
+          ),
+          if (isLoading)
+            Center(
+              child: CircularProgressIndicator(),
+            ),
+        ],
       ),
     );
   }
@@ -102,34 +130,38 @@ class ForgotPasswordPage extends StatelessWidget {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text(
-              'cant be empty!'), // Giả sử trans.enterEmail là thông báo "Vui lòng nhập email"
+              ''), // Giả sử trans.enterEmail là thông báo "Vui lòng nhập email"
           backgroundColor: Colors.red,
         ),
       );
       return;
     }
 
-    if (email != null) {
-      try {
-        await resetPassword(email);
-        // Hiển thị thông báo thành công và chuyển trang
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(trans.sendResetPasswordTo + ' $email'),
-            backgroundColor: Colors.green,
-          ),
-        );
-        pushWithNavBar(
-            context, MaterialPageRoute(builder: (context) => LoginPage()));
-      } catch (e) {
-        // Hiển thị thông báo lỗi
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text("Error: ${e.toString()}"),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
+    setState(() {
+      isLoading = true;
+    });
+
+    try {
+      await resetPassword(email);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(trans.sendResetPasswordTo + ' $email'),
+          backgroundColor: Colors.green,
+        ),
+      );
+      pushWithNavBar(
+          context, MaterialPageRoute(builder: (context) => LoginPage()));
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text("Error: ${e.toString()}"),
+          backgroundColor: Colors.red,
+        ),
+      );
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
     }
   }
 }
