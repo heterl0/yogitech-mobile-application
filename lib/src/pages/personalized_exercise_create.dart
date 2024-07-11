58b47c4159f87210293ba79962cf008245e6b809
Chinhphu <doraemon.com56@gmail.com>
2024-07-11 17:11:44 +0700

    Push chơi chứ chưa xong

--- lib/src/pages/personalized_exercise_create.dart
+++ lib/src/pages/personalized_exercise_create.dart
@@ -1,3 +1,9 @@
+import 'package:YogiTech/api/dioInstance.dart';
+import 'package:YogiTech/src/custombar/bottombar.dart';
+import 'package:YogiTech/src/models/pose.dart';
+import 'package:YogiTech/src/widgets/dropdown_field.dart';
+import 'package:YogiTech/utils/formatting.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:YogiTech/src/custombar/appbar.dart';
 import 'package:YogiTech/src/shared/styles.dart';
@@ -5,6 +11,24 @@ import 'package:YogiTech/src/shared/app_colors.dart';
 import 'package:YogiTech/src/widgets/box_input_field.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:persistent_bottom_nav_bar_v2/persistent_bottom_nav_bar_v2.dart';
+
+Future<List<Pose>> getPoses() async {
+  try {
+    final url = formatApiUrl('/api/v1/poses/');
+    final Response response = await DioInstance.get(url);
+    if (response.statusCode == 200) {
+      List<Pose> data =
+          (response.data as List).map((e) => Pose.fromMap(e)).toList();
+      return data;
+    } else {
+      print('Get poses failed with status code: ${response.statusCode}');
+      return [];
+    }
+  } catch (e) {
+    print('Get poses error: $e');
+    return [];
+  }
+}
 
 class PersonalizedExerciseCreatePage extends StatefulWidget {
   const PersonalizedExerciseCreatePage({super.key});
@@ -16,18 +40,120 @@ class PersonalizedExerciseCreatePage extends StatefulWidget {
 
 class _PersonalizedExerciseCreatePageState
     extends State<PersonalizedExerciseCreatePage> {
-  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _titleController = TextEditingController();
   final TextEditingController _difficultyController = TextEditingController();
   final TextEditingController _caloriesController = TextEditingController();
-  final TextEditingController _imageController = TextEditingController();
+
+  final TextEditingController _benefitsController = TextEditingController();
+  final TextEditingController _pointController = TextEditingController();
+
+  List<Pose> _poses = [];
+  List<Pose> _selectedPoses = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchPoses();
+  }
 
   @override
   void dispose() {
-    _nameController.dispose();
+    _titleController.dispose();
     _difficultyController.dispose();
     _caloriesController.dispose();
-    _imageController.dispose();
+
+    _benefitsController.dispose();
+    _pointController.dispose();
     super.dispose();
+  }
+
+  Future<void> _fetchPoses() async {
+    final poses = await getPoses();
+    setState(() {
+      _poses = poses;
+    });
+  }
+
+  void _onPoseSelected(Pose pose) {
+    setState(() {
+      if (_selectedPoses.contains(pose)) {
+        _selectedPoses.remove(pose);
+      } else {
+        _selectedPoses.add(pose);
+      }
+    });
+  }
+
+  void _showPoseSelectionDialog() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(AppLocalizations.of(context)!.selectPoses),
+          content: Container(
+            width: double.maxFinite,
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: _poses.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 3 / 3,
+              ),
+              itemBuilder: (context, index) {
+                final pose = _poses[index];
+                final isSelected = _selectedPoses.contains(pose);
+                final TextEditingController _durationController =
+                    TextEditingController();
+
+                return GestureDetector(
+                  onTap: () => _onPoseSelected(pose),
+                  child: GridTile(
+                    header: GridTileBar(
+                      title: Text(pose.name),
+                      subtitle: Text(pose.level.toString()),
+                      backgroundColor:
+                          isSelected ? Colors.black45 : Colors.transparent,
+                    ),
+                    footer: GridTileBar(
+                      title: Text('${pose.calories} calories'),
+                      backgroundColor: Colors.black45,
+                    ),
+                    child: Column(
+                      children: [
+                        Image.network(
+                          pose.image_url,
+                          fit: BoxFit.cover,
+                        ),
+                        TextField(
+                          controller: _durationController,
+                          decoration: InputDecoration(
+                            labelText: AppLocalizations.of(context)!.duration,
+                          ),
+                          keyboardType: TextInputType.number,
+                          onChanged: (value) {
+                            pose.duration = int.tryParse(value)!;
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+          actions: [
+            TextButton(
+              child: Text(AppLocalizations.of(context)!.choose),
+              onPressed: () {
+                setState(() {});
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -37,41 +163,84 @@ class _PersonalizedExerciseCreatePageState
     return Scaffold(
       backgroundColor: theme.colorScheme.surface,
       appBar: CustomAppBar(
-        title: trans.yourExercise,
+        title: trans.createExercise,
         style: widthStyle.Large,
       ),
       body: SingleChildScrollView(
         child: Container(
           margin: const EdgeInsets.all(24.0),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
-                trans.exercise,
+                trans.title,
                 style: h3.copyWith(color: theme.colorScheme.onPrimary),
               ),
+              const SizedBox(height: 12),
               BoxInputField(
-                controller: _nameController,
+                controller: _titleController,
               ),
-              const SizedBox(height: 8),
-              BoxInputField(
+              const SizedBox(height: 12),
+              Text(
+                trans.level,
+                style: h3.copyWith(color: theme.colorScheme.onPrimary),
+              ),
+              const SizedBox(height: 12),
+              CustomDropdownFormField(
                 controller: _difficultyController,
+                items: ['Dễ', 'Thường', 'Khó'],
               ),
-              BoxInputField(
-                controller: _caloriesController,
+              const SizedBox(height: 12),
+              Text(
+                trans.poses,
+                style: h3.copyWith(color: theme.colorScheme.onPrimary),
               ),
-              BoxInputField(
-                controller: _imageController,
+              const SizedBox(height: 12),
+              Wrap(
+                spacing: 8.0,
+                runSpacing: 4.0,
+                children: _selectedPoses
+                    .map((pose) => Chip(
+                          label: Text('${pose.name} (${pose.duration} min)'),
+                          onDeleted: () {
+                            setState(() {
+                              _selectedPoses.remove(pose);
+                            });
+                          },
+                        ))
+                    .toList(),
               ),
-              SizedBox(height: 24),
+              const SizedBox(height: 12),
               ElevatedButton(
-                onPressed: () {
-                  // Handle the save action
-                },
-                child: Text(trans.save),
+                onPressed: _showPoseSelectionDialog,
+                child: Text(trans.selectPoses),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                trans.benefits,
+                style: h3.copyWith(color: theme.colorScheme.onPrimary),
+              ),
+              const SizedBox(height: 12),
+              BoxInputField(
+                controller: _benefitsController,
+              ),
+              const SizedBox(height: 12),
+              Text(
+                trans.point,
+                style: h3.copyWith(color: theme.colorScheme.onPrimary),
+              ),
+              const SizedBox(height: 12),
+              BoxInputField(
+                controller: _pointController,
+                keyboardType: TextInputType.number,
               ),
             ],
           ),
         ),
+      ),
+      bottomNavigationBar: CustomBottomBar(
+        buttonTitle: trans.create,
+        onPressed: () {},
       ),
     );
   }
