d58d3f152857aebab09c7f7d10a995522b52800c
Hieu Le Van <lehieu99666@gmail.com>
2024-07-29 16:59:11 +0700

    feat: Add TextToSpeech functionality for yoga instructions

--- android/app/src/main/kotlin/com/example/yogi_application/fragment/ScoreFragment.kt
+++ android/app/src/main/kotlin/com/example/yogi_application/fragment/ScoreFragment.kt
@@ -5,6 +5,7 @@ import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.speech.tts.TextToSpeech
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
@@ -21,8 +22,15 @@ import com.example.yogi_application.MainViewModel
 import com.example.yogi_application.R
 import com.example.yogi_application.databinding.FragmentScoreBinding
 import com.example.yogi_application.model.PoseLogResult
+import java.util.Locale
 
-class ScoreFragment: Fragment(R.layout.fragment_score) {
+class ScoreFragment: Fragment(R.layout.fragment_score), TextToSpeech.OnInitListener {
+
+    private lateinit var tts: TextToSpeech
+    private val localeEN = Locale("en", "US")
+    private val localeVI = Locale("vi", "VN")
+    private var isGiveSuggest = false;
+
     private var _fragmentScoreBinding: FragmentScoreBinding? = null
 
     private val fragmentCameraBinding
@@ -46,10 +54,33 @@ class ScoreFragment: Fragment(R.layout.fragment_score) {
     ): View {
         _fragmentScoreBinding = FragmentScoreBinding.inflate(inflater, container, false);
         setHasOptionsMenu(true)  // Enable options menu in fragment
-
+        tts = TextToSpeech(requireContext(), this)
         return _fragmentScoreBinding!!.root;
     }
 
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            // Set default language to English
+            val result = tts.setLanguage(localeEN)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS", "Language not supported")
+            } else {
+                // Set the pitch and speech rate suitable for yoga instructions
+                tts.setPitch(1.0f) // Normal pitch
+                tts.setSpeechRate(0.8f) // Slightly slower speech rate
+            }
+        } else {
+            Log.e("TTS", "Initialization failed")
+        }
+    }
+
+
+
+    private fun speak(text: String, language: Locale) {
+        tts.language = language
+        tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "")
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -123,6 +154,10 @@ class ScoreFragment: Fragment(R.layout.fragment_score) {
                             _fragmentScoreBinding!!.result.text = "0%";
                             _fragmentScoreBinding!!.description.text =
                                 getString(R.string.adjust)
+                            if (isGiveSuggest == false) {
+                                speak(getString(R.string.adjust), localeEN);
+                                isGiveSuggest = true;
+                            }
                         } else {
                             _fragmentScoreBinding!!.result.text =
                                 "${result.data?.score?.toInt().toString()}%";
@@ -211,6 +246,10 @@ class ScoreFragment: Fragment(R.layout.fragment_score) {
 
     override fun onDestroyView() {
         super.onDestroyView()
+        if (tts != null) {
+            tts.stop()
+            tts.shutdown()
+        }
         _fragmentScoreBinding = null
         countDownTimer?.cancel()
     }
